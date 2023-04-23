22b14eb476d2ba1d3d09fef7594f463137c6cbe7
Filip <dsw51560@student.dsw.edu.pl>
2023-04-23 18:27:29 +0200

    czwarty commit

--- tt.sh
+++ tt.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 use_date=false
-
+create_logs=false
 
 while [ $# -gt 0 ]; do
     case "$1" in
         --date)
             use_date=true
+            ;;
+        --logs)
+            create_logs=true
             ;;
         *)
             echo "Nieznana opcja: $1"
@@ -16,9 +19,20 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-
+#~~~
 if [ "$use_date" = true ]; then
     echo "Dzisiejsza data to: $(date +%d.%m.%Y)"
-else
-    echo "Użycie: $0 --date"
+fi
+
+#~~~
+if [ "$create_logs" = true ]; then
+    for i in {1..100}; do
+        touch "log$i.txt"
+        echo "Utworzono plik log$i.txt"
+    done
+fi
+
+#~~
+if [ "$use_date" = false ] && [ "$create_logs" = false ]; then
+    echo "Użycie: $0 --date --logs"
 fi
