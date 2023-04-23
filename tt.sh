3549bfa6c787c0458b6677363bcba14676a5c7d3
Filip <dsw51560@student.dsw.edu.pl>
2023-04-23 18:33:45 +0200

    Dodano flagę --help która wyświetla dostępne opcje oraz upiększono skrypt

--- tt.sh
+++ tt.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Ustawienie domyślnych wartości
 use_date=false
 create_logs=false
 
+# Obsługa flag
 while [ $# -gt 0 ]; do
     case "$1" in
         --date)
@@ -10,6 +12,13 @@ while [ $# -gt 0 ]; do
             ;;
         --logs)
             create_logs=true
+            ;;
+        --help)
+            echo "Użycie: $0 [OPCJA]"
+            echo "Dostępne opcje:"
+            echo "--date    Wyświetla dzisiejszą datę"
+            echo "--logs    Tworzy 100 plików log.txt"
+            exit 0
             ;;
         *)
             echo "Nieznana opcja: $1"
@@ -19,12 +28,12 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-#~~~
+# Wyświetlanie daty
 if [ "$use_date" = true ]; then
-    echo "Dzisiejsza data to: $(date +%d.%m.%Y)"
+    echo "Dzisiejsza data to: $(date +%Y-%m-%d)"
 fi
 
-#~~~
+# Tworzenie plików log.txt
 if [ "$create_logs" = true ]; then
     for i in {1..100}; do
         touch "log$i.txt"
@@ -32,7 +41,10 @@ if [ "$create_logs" = true ]; then
     done
 fi
 
-#~~
+# Komunikat, gdy nie podano żadnej flagi
 if [ "$use_date" = false ] && [ "$create_logs" = false ]; then
-    echo "Użycie: $0 --date --logs"
+    echo "Użycie: $0 [OPCJA]"
+    echo "Dostępne opcje:"
+    echo "--date    Wyświetla dzisiejszą datę"
+    echo "--logs    Tworzy 100 plików log.txt"
 fi
